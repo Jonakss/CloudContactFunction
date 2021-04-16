7da22fcf5e261dd207f8b710011d382ccb3b244c
Jonathan <jona.correapc@gmail.com>
2021-04-16 00:58:34 -0300

    Fixed send mail

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const sgMail = require('@sendgrid/mail');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-var email = "contacto@constructoralar.uy",
-    email_to = "ventas@constructoralar.uy"
-    subject = "Contacto - Presupuesto";
-/*
-    body {
-        replay_to
-        message
-    }
-*/
+    var email = process.env.EMAIL,
+        email_to = process.env.EMAIL_TO,
+        subject = process.env.SUBJECT;
+    /*
+        body {
+            replay_to
+            message
+        }
+    */
 
-exports.contactFunction = (req, res) => {
-    const msg = {
-      to: email_to, // Change to your recipient
-      replay_to: {
-            email: replay_to,
-            name: name
-      },
-      from: email, // Change to your verified sender
-      subject: subject,
-      text: message,
-      html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    }
-    sgMail
-      .send(msg)
-      .then(() => {
-        console.log('Email sent');
-        res.send(200);
-      })
-      .catch((error) => {
-        console.error(error)
-        res.send(500);
-      });
-};
+    exports.contactFunction = (req, res) => {
+        ({name, replay_to, message} = req.body);
+        console.log(req.body.toString());
+        var msg = {
+            "personalizations": [{
+                "to": [{
+                    "email": email_to
+                }]
+            }],
+            "from": {
+                "email": email
+            },
+            "replay_to" : {
+                "email" : replay_to
+            },
+            "subject": subject,
+            "content": [{
+                "type": "text/plain",
+                "value": message
+            }]
+        
+        };
+        sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Email sent');
+            res.sendStatus(200);
+        })
+        .catch((error) => {
+            console.error(error.toString())
+            res.sendStatus(500);
+        });
+    };
